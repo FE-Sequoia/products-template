89319d57039f380af130ea36bbb999acb1fb64ed
sequoia <sequoia.tungfang@gmail.com>
2024-05-04 16:45:48 +0800

    del: 删除主分支测试代码

--- code/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ "$1" = "build" ];then
-    mkdir /home/changsheng/project/project94756/project
-    cp -a /home/changsheng/project/project94756/server/. /home/changsheng/project/project94756/project/
-    cd /home/changsheng/project/project94756/project
-    rm -rf /home/changsheng/project/project94756/server
-    echo "执行成功"
-fi
